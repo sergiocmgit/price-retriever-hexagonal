248dc3897f182c0a14590f7052764c831a1bfe3d
sergiocostamoreno <47816978+sergiocostamoreno@users.noreply.github.com>
2024-02-10 11:11:56 +0100

    TODOs added to Amount

--- src/main/kotlin/com/scosta/priceretrieverhexagonal/application/domain/Amount.kt
+++ src/main/kotlin/com/scosta/priceretrieverhexagonal/application/domain/Amount.kt
@@ -3,6 +3,8 @@ package com.scosta.priceretrieverhexagonal.application.domain
 import java.math.BigDecimal
 import java.util.Currency
 
+// TODO: include currency within the value object
+// TODO: stop using java.util.Currency
 data class Amount(
     val value: BigDecimal,
     val currency: Currency
