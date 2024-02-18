c031e981b505f125e797f75ce007518a21abc6db
sergiocostamoreno <47816978+sergiocostamoreno@users.noreply.github.com>
2024-02-18 17:57:46 +0100

    Timestamps without zone

--- src/test/kotlin/com/scosta/priceretrieverhexagonal/fixtures/PriceBuilders.kt
+++ src/test/kotlin/com/scosta/priceretrieverhexagonal/fixtures/PriceBuilders.kt
@@ -19,12 +19,12 @@ import kotlin.random.Random
 val DEFAULT_PRICE_ID = Random.nextLong()
 const val DEFAULT_PRODUCT_ID = "some-product-id"
 const val DEFAULT_BRAND_ID = 123L
-val DEFAULT_START_AT: OffsetDateTime = OffsetDateTime.parse("2024-02-10T12:08:48+01:00")
+val DEFAULT_START_AT: OffsetDateTime = OffsetDateTime.parse("2024-02-10T11:08:48Z")
 val DEFAULT_END_AT: OffsetDateTime = DEFAULT_START_AT.plusHours(1)
 val DEFAULT_APPLIED_AT: OffsetDateTime = DEFAULT_START_AT.plusMinutes(30)
 const val DEFAULT_PRICE_LIST = "some-price-list"
 const val DEFAULT_PRIORITY = 1
-val DEFAULT_AMOUNT: BigDecimal = ONE
+val DEFAULT_AMOUNT: BigDecimal = ONE.setScale(2)
 
 fun buildPrice() = Price(
     id = Id(DEFAULT_PRICE_ID),
